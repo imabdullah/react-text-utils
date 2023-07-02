6064ebffa329136cd0ccf5aa70347c659b31bcb7
imabdullah <imuhammadabdullah@outlook.com>
2023-07-02 20:56:37 +0500

    text utils functions with color changing themes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Navbar from './components/Navbar';
+import { TextForm } from './components/TextForm';
+import React, { useState } from 'react'
+import { Alter } from './components/Alter';
 function App() {
+
+  const bgColors = [
+    {
+      name: 'Blue',
+      class: 'primary',
+      text: 'light'
+    },
+    {
+      name: 'Green',
+      class: 'success',
+      text: 'light'
+    },
+    {
+      name: 'Red',
+      class: 'danger',
+      text: 'light'
+    },
+    {
+      name: 'Orange',
+      class: 'warning',
+      text: 'light'
+    },
+    {
+      name: 'Light Blue',
+      class: 'info',
+      text: 'dark'
+    },
+    {
+      name: 'Black',
+      class: 'dark',
+      text: 'light'
+    }];
+
+  const [mode, setMode] = useState('light');
+
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (msg, type) => {
+    setAlert({
+      msg: msg,
+      type: type
+    })
+
+    setTimeout(function () {
+      setAlert(null)
+    }, 1500)
+  }
+
+  const toogleMode = (e) => {
+
+    console.log(e.target.getAttribute('data-color'));
+    if (mode === 'light') {
+      setMode(e.target.getAttribute('data-color'))
+      document.body.classList.add('bg-' + e.target.getAttribute('data-color'));
+      document.body.classList.add('bg-opacity-50')
+      showAlert('Dark Mode enabled', 'success');
+    } else {
+      document.body.classList.remove('bg-' + mode);
+      setMode('light')
+
+      //document.body.style.backgroundColor = "white";
+      showAlert('Light Mode enabled', 'success');
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar mode={mode} toogleMode={toogleMode} bgColors={bgColors} />
+
+      <div className="container mt-4">
+        {/* <Alter alert={alert} /> */}
+        <TextForm
+          mode={mode}
+          title="Simply enter your text and choose the case you want to convert it to."
+          label="Type your text"
+          showAlert={showAlert}
+        />
+      </div>
+    </>
   );
 }
 
---
+++ src/components/TextForm.js
@@ -0,0 +1,123 @@
+
+import React, { useState } from 'react'
+import PropTypes from 'prop-types';
+
+export const TextForm = (props) => {
+
+    const [text, setText] = useState("Type your text");
+
+    const [speakToogle, setSpeak] = useState(false);
+
+    const handleOnChange = (e) => {
+        setText(e.target.value);
+
+    }
+
+    const converToUppercase = () => {
+        setText(text.toUpperCase());
+    }
+    const convertToLowercase = () => {
+        setText(text.toLocaleLowerCase())
+    }
+
+    const copythis = () => {
+        navigator.clipboard.writeText(text);
+        props.showAlert('Copied to Clipboard', 'info')
+    }
+
+    const converToTitleCase = () => {
+
+    }
+
+
+
+
+    const converToCaptalize = () => {
+        let v = text.replace(
+            /\w\S*/g,
+            function (txt) {
+                return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+            }
+        );
+
+        setText(v);
+    }
+
+
+    const speakIt = () => {
+        let msg = new SpeechSynthesisUtterance();
+        msg.text = text;
+        console.log(text);
+        if (!speakToogle) {
+            window.speechSynthesis.speak(msg);
+            setSpeak(true)
+
+            //count length of text
+            const mintuesToReadAWords = text.split(' ').length * (0.008) //0.008 mins per word to read 
+
+
+
+
+            setTimeout(function () {
+                setSpeak(false)
+            }, mintuesToReadAWords * 60000);
+
+        }
+        else {
+            window.speechSynthesis.cancel()
+            setSpeak(false)
+        }
+
+    }
+
+
+    return (
+        <>
+            <div className="container" style={{ color: props.mode == 'light' ? 'black' : 'white' }}>
+                <h4>{props.title}</h4>
+                <button className="btn btn-secondary mt-2" onClick={copythis}> Copy to Clipboard</button>
+                <button className="btn btn-secondary mt-2  mx-2" onClick={speakIt}>{!speakToogle ? 'Speak it' : 'Stop Speaking'}</button>
+
+                <div className="form-floating">
+                    <textarea className={`form-control bg-${props.mode} bg-opacity-10`}
+                        value={text}
+                        onChange={handleOnChange}
+                        id="floatingTextarea2"
+                        style={{
+                            border: "1px solid",
+                            color: props.mode == 'light' ? 'black' : 'white',
+                            height: "200px"
+                        }}
+                    ></textarea>
+                    <label htmlFor="floatingTextarea2">{props.label}</label>
+                </div>
+
+
+                <button className="btn btn-primary mt-2" onClick={converToUppercase}>Convert to Uppercase</button>
+                <button className="btn btn-primary mt-2 mx-2" onClick={convertToLowercase}>Convert to Lowercase</button>
+
+
+                <button className="btn btn-primary mt-2 mx-2" onClick={converToCaptalize}>Captalize it</button>
+                <button className="btn btn-primary mt-2 mx-2" onClick={converToTitleCase}>Title Case</button>
+
+                <div className="mt-4">
+                    <h4>Text Summary</h4>
+                    <p>
+                        {text.split(" ").length} words and {text.length} characters
+                    </p>
+                </div>
+
+            </div>
+        </>
+    )
+}
+
+TextForm.propTypes = {
+    title: PropTypes.string,
+    label: PropTypes.string.isRequired
+}
+
+TextForm.defaultProps = {
+    title: "Type Text",
+    label: "Type Text"
+}
